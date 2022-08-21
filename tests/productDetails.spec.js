abcae8adf57099b848422cadb461ca9e40dc4f52
Joao Victor <imashinzo10@gmail.com>
2022-08-21 13:15:32 -0300

    Requisito 5 finalizado e 6 iniciado mas nao testado

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -31,13 +31,15 @@ const productDetails = require('../src/productDetails');
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    fail('Teste vazio!');
     // ESCREVA SEUS TESTES ABAIXO:
-    // Teste se productDetails é uma função.
-    // Teste se o retorno da função é um array.
-    // Teste se o array retornado pela função contém dois itens dentro.
-    // Teste se os dois itens dentro do array retornado pela função são objetos.
-    // Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.
-    // Teste se os dois productIds terminam com 123.
+    // refencia do toBeInstanceOf = https://jestjs.io/pt-BR/docs/expect#tobeinstanceofclass //
+    expect(typeof productDetails).toBe('function');
+    expect(Array.isArray(productDetails('Alcool gel', 'Máscara'))).toBe(true);
+    expect(productDetails('Alcool gel', 'Máscara').length).toBe(2);
+    expect(productDetails('Alcool gel', 'Máscara')[0]).toBeInstanceOf(Object);
+    expect(productDetails('Alcool gel', 'Máscara')[1]).toBeInstanceOf(Object);
+    expect(productDetails('Alcool gel', 'Máscara')[0]).not.toBe(productDetails('Alcool gel', 'Máscara')[1]);
+    expect(productDetails('Alcool gel', 'Máscara')[0].details.productId).toMatch('123');
+    expect(productDetails('Alcool gel', 'Máscara')[1].details.productId).toMatch('123');
   });
 });
